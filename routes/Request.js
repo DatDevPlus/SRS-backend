8950d0eca0b898bdc888077217c3f05e0e4428a3
nhanVoThanhdp05 <nhan.vo_dp05@devplus.edu.vn>
2023-04-19 15:34:41 +0700

    feat: dpm-add-login-with-google-route

--- routes/Request.js
+++ routes/Request.js
@@ -9,16 +9,10 @@ import {
 import { VerifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
-
 router.get("/:id",VerifyToken, Get_Request_Detail);
-//get
 router.get("/", VerifyToken, Get_All_Request);
-//create
 router.post("/",VerifyToken, Create_Request);
-//Update
 router.put("/:id",VerifyToken, Update_Request);
-
-//delete
 router.delete("/:id",VerifyToken, Delete_Request);
 
 export default router;
