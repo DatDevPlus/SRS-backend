afba9c84bdaec93f51a33989dbf2533f6ba820a9
anhhuy0168 <anhhuy123zxc123@gmail.com>
2023-04-19 17:49:54 +0700

    feat/dpm-getOneGroup

--- controllers/GroupController.js
+++ controllers/GroupController.js
@@ -11,8 +11,8 @@ export const get_All_Groups = async (req, res) => {
 };
 export const get_Group = async (req, res) => {
   try {
-    const group = await Group.find().sort([["createdAt", -1]]);
-    res.json({ success: true, group });
+    const group = await Group.findById(req.params.id);
+    res.status(200).json(group);
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Internal server error" });
@@ -43,16 +43,16 @@ export const addID = async (req, res) => {
   try {
     const { masters_id, staffs_id } = req.body;
     const group = await Group.findById(req.params.id);
-  //   const existingMasterId = Array.from(group.masters_id).some((id) =>
-  //   id.equals(masters_id)
-  // );
-  // const existingStaffId = Array.from(group.staffs_id).some((id) =>
-  //   id.equals(staffs_id)
-  // );
-  
-  // if (existingMasterId || existingStaffId) {
-  //   return res.status(400).json({ msg: "ID already exists" });
-  // }
+    //   const existingMasterId = Array.from(group.masters_id).some((id) =>
+    //   id.equals(masters_id)
+    // );
+    // const existingStaffId = Array.from(group.staffs_id).some((id) =>
+    //   id.equals(staffs_id)
+    // );
+
+    // if (existingMasterId || existingStaffId) {
+    //   return res.status(400).json({ msg: "ID already exists" });
+    // }
 
     group.masters_id.push(masters_id);
     group.staffs_id.push(staffs_id);
