f2e3e12e9b754fab5807f334e9d8507017e43bea
nhanVoThanhdp05 <nhan.vo_dp05@devplus.edu.vn>
2023-04-20 18:27:21 +0700

    feat: dpm-fix-add-remove-user-permission-feature

--- routes/Auth.js
+++ routes/Auth.js
@@ -6,17 +6,20 @@ import {
   addPermission,
   addRole,
   loginGoogle,
+  removePermission,
 
 } from "../controllers/AuthController.js";
 import { VerifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/", VerifyToken, checkUser);
-router.post("/register", VerifyToken, register);
 router.post("/login", login);
 router.post("/login-with-google", loginGoogle);
+router.post("/register", register);
+
+router.get("/", VerifyToken, checkUser);
 router.post("/:id", VerifyToken, addRole);
 router.post("/add-permission/:id", VerifyToken, addPermission);
+router.post("/remove-permission/:id", VerifyToken, removePermission);
 
 export default router;
