d167bd654f6229d4aef357e6b63d33e808c629f7
Nguyễn Anh Huy <126643590+nguyenanhhuy0169@users.noreply.github.com>
2023-04-24 09:14:12 +0700

    DPM-Workspace (#9)

--- controllers/DayoffController.js
+++ controllers/DayoffController.js
@@ -1,152 +1,48 @@
-// import Request_detail from "../models/RequestDetail.js";
-// export const Get_Request_Detail = async (req, res) => {
-//   try {
-//     const request = await Request_detail.findById({
-//       _id: req.params.id,
-//     });
-//     if (!request) {
-//       res.status(404).json({ success: false, message: "Request is not exist" });
-//     }
-//     if (request) res.json({ success: true, request });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, message: "Internal server error" });
-//   }
-// };
-// export const Get_All_Request = async (req, res) => {
-//   try {
-//     const request = await Request_detail.find().sort([["createdAt", -1]]);
-//     res.json({ success: true, request });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, message: "Internal server error" });
-//   }
-// };
-// export const Create_Request = async (req, res) => {
-//   try {
-//     const {
-//       reason,
-//       quantity,
-//       start_date,
-//       end_date,
-//       user_id,
-//       day_off_type,
-//       day_off_time,
-//       status,
-//       approvers_number,
-//     } = req.body;
-//     if (
-//       !reason ||
-//       !quantity ||
-//       !start_date ||
-//       !end_date ||
-//       !user_id ||
-//       !day_off_type ||
-//       !day_off_time ||
-//       !status ||
-//       !approvers_number
-//     )
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "Missing information" });
-//     const newRequest = new Request_detail({
-//       reason,
-//       quantity,
-//       start_date,
-//       end_date,
-//       user_id: req.userId,
-//       day_off_type,
-//       day_off_time,
-//       status,
-//       approvers_number,
-//     });
-//     await newRequest.save();
-//     res.json({
-//       success: true,
-//       message: "Create complete !",
-//       category: newRequest,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, message: "Internal server error" });
-//   }
-// };
-// export const Update_Request = async (req, res) => {
-//   const {
-//     reason,
-//     quantity,
-//     start_date,
-//     end_date,
-//     user_id,
-//     date_off_time,
-//     date_off_type,
-//     status,
-//     approvers_number,
-//   } = req.body;
-//   if (
-//     !reason ||
-//     !quantity ||
-//     !start_date ||
-//     !end_date ||
-//     !user_id ||
-//     !date_off_time ||
-//     !date_off_type ||
-//     !status ||
-//     !approvers_number
-//   )
-//     return res
-//       .status(400)
-//       .json({ success: false, message: "Missing information" });
-//   try {
-//     let updateRequest = {
-//       reason,
-//       quantity,
-//       start_date,
-//       end_date,
-//       user_id:req.userId,
-//       date_off_time,
-//       date_off_type,
-//       status,
-//       approvers_number,
-//     };
-//     const updateRequestCondition = { _id: req.params.id };
-//     updateRequest = await Request_detail.findByIdAndUpdate(
-//       updateRequestCondition,
-//       updateRequest,
-//       { new: true }
-//     );
-//     if (!updateRequest)
-//       return res.status(401).json({
-//         success: false,
-//         message: "Request not found",
-//       });
 
-//     res.json({
-//       success: true,
-//       message: "Excellent progress!",
-//       req: updateRequest,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, message: "Internal server error" });
-//   }
-// };
+import Request_detail from "../models/RequestDetail.js";
 
-// export const Delete_Request = async (req, res) => {
-//   try {
-//     const requestDeleteCondition = { _id: req.params.id };
-//     const deleteRequest = await Request_detail.findOneAndDelete(
-//       requestDeleteCondition
-//     );
+export const Get_All_DayOff = async (req, res) => {
+  try {
+    const request = await Request_detail.find({ status: "approved" }).sort([
+      ["status"],
+    ]);
+    console.log(request);
+    res.json({ success: true, request });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
 
-//     if (!deleteRequest)
-//       return res.status(401).json({
-//         success: false,
-//         message: "Request not found ",
-//       });
-//     res.json({ success: true, req: deleteRequest });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, message: "Internal server error" });
-//   }
-// };
+export const revertDayOff = async (req, res) => {
+  try {
+    const { reason } = req.body;
+    // Simple validation
+    if (!reason)
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing information !" });
+
+    let revert = {
+      reason,
+      status: "pending",
+    };
+    const revertCondition = { _id: req.params.id };
+    revert = await Request_detail.findByIdAndUpdate(revertCondition, revert, {
+      new: true,
+    });
+    if (!revert)
+      return res.status(401).json({
+        success: false,
+        message: "Group not found",
+      });
+    res.json({
+      success: true,
+      message: "Excellent progress!",
+      revert: revert,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
