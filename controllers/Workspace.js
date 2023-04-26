d8968f719a3c59103793e2ccc93e716d983f8d5a
anhhuy0168 <anhhuy123zxc123@gmail.com>
2023-04-26 15:45:59 +0700

    feat:dpm-fix-workspace

--- controllers/Workspace.js
+++ controllers/Workspace.js
@@ -67,10 +67,10 @@ export const Create_Workspace = async (req, res) => {
 };
 export const Update_Workspace = async (req, res) => {
   const { workspace_name, description, manager_id = [], status } = req.body;
-  if (manager_id.length<0)
+  if (manager_id.length<=0)
     return res
       .status(400)
-      .json({ success: false, message: "Missing information" });
+      .json({ success: false, message: "Need at least one manager in the workspace !!!" });
   try {
     let updateWorkspace = {
       workspace_name,
